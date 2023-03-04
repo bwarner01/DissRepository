72dd43b35ade0e74a048a45f62bcc4398d8d1192
bwarner01 <b9022438@newcastle.ac.uk>
2023-03-04 13:32:59 +0000

    Begin implementation of hard coded AI

---
+++ Monopoly/StatisticalAgent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class StatisticalAgent
+    {
+        private Random picker = new Random();
+        private int stockpileValue = 0;
+
+        public void CalculateStockpile(List<Property> board)
+        {
+            
+        }
+
+        public int SelectOption(List<string> options, Player p)
+        {
+
+        }
+
+        public int SelectItem(int range, Player p)
+        {
+
+        }
+    }
+}
