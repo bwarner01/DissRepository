89bcfba851c749c1e4af0130a19e429a7d99fa49
bwarner01 <b9022438@newcastle.ac.uk>
2023-03-12 15:44:16 +0000

    Begin implementation of RL agent

---
+++ Monopoly/RLAgent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class RLAgent
+    {
+        private double epsilon;
+        private double alpha;
+        private double gamma;
+    }
+
+    public class State
+    {
+        private int pPosition {  get; set; }
+        private int pMoney {  get; set; }
+        private List<Property> ownedProperties { get; set; }
+        private List<Property> board { get; set; }
+        private List<Player> playerList { get; set; }
+
+        public State(int pPosition, int pMoney, List<Property> ownedProperties, List<Property> board, List<Player> playerList) 
+        {
+            this.pPosition = pPosition;
+            this.pMoney = pMoney;
+            this.ownedProperties = ownedProperties;
+            this.board = board;
+            this.playerList = playerList;
+        }
+    }
+}
