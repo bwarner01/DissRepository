207c0174c85ec51890213c7c2c087d25d9673ac7
bwarner01 <b9022438@newcastle.ac.uk>
2023-04-04 17:51:40 +0100

    Continue implementation of RL agent

--- Monopoly/RLAgent.cs
+++ Monopoly/RLAgent.cs
@@ -3,21 +3,237 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tensorflow;
+using NeuronDotNet;
+using NeuronDotNet.Core;
+using NeuronDotNet.Core.Backpropagation;
+using NeuronDotNet.Core.Initializers;
 
 namespace Monopoly
 {
     public class RLAgent
     {
-        private TF_Tensor inputTensor;
-        
+
+        public State lastState { get; set; }
+        public string lastAction { get; set; }
+
+        public List<string> allActions = new List<string>();
+
+        public int epoch;
+
         private double epsilon { get; set; }
         private double alpha { get; set; }
         private double gamma { get; set; }
+        private double lamda { get; set; }
 
-        Dictionary<State, Dictionary<string, double>> qTable = new Dictionary<State, Dictionary<string, double>>();
+        public List<Trace> traces = new List<Trace>();
+
+        NeuronDotNet.Core.Network network;
 
 
+        public RLAgent(int inputCount) 
+        {
+            LinearLayer inputLayer = new LinearLayer(inputCount + 1);
+            SigmoidLayer hiddenLayer = new SigmoidLayer(150);
+            LinearLayer outputLayer = new LinearLayer(1);
+
+            new BackpropagationConnector(inputLayer, hiddenLayer).Initializer = new RandomFunction(-0.5, 0.5);
+            new BackpropagationConnector(hiddenLayer, outputLayer).Initializer = new RandomFunction(-0.5, 0.5);
+
+            this.network = new BackpropagationNetwork(inputLayer, outputLayer);
+
+            this.network.SetLearningRate(0.2);
+            this.network.Initialize();
+
+
+            this.epsilon = 0.5;
+            this.alpha = 0.2;
+            this.gamma = 0.95;
+            this.lamda = 0.8;
+
+            epoch = 1;
+
+            allActions.Add("Make Trade");
+            allActions.Add("Roll The Dice");
+            allActions.Add("Buy Property");
+            allActions.Add("Roll Dice To Get Out Of Jail");
+            allActions.Add("Pay To Get Out Of Jail");
+            allActions.Add("Use Get Out Jail Free Card");
+            allActions.Add("Mortgage/Sell Property");
+            allActions.Add("Unmortgage Property");
+            allActions.Add("Build Houses");
+            allActions.Add("Sell Houses");
+            allActions.Add("End Turn");
+        }
+
+        public string FirstAction(State state, Dictionary<List<string>,float> actions, List<string> options)
+        {
+            epoch++;
+
+            string action = "";
+
+            Dictionary<string, double> QValues = CalculateQValues(state);
+
+            action = EpsilonGreedy(QValues, options);
+
+            lastAction = action;
+            lastState = state;
+
+            traces.Add(new Trace(state.ToVector(), action, 1));
+
+            return action;
+        }
+
+        public string SelectAction(State state, double reward, ref Dictionary<string, double> actions, List<string> options)
+        {
+            epoch++;
+
+            string action = "";
+
+            Dictionary<string, double> QValues = CalculateQValues(state);
+
+            action = EpsilonGreedy(QValues, options);
+
+            double QValue = 0;
+            bool exists = false;
+
+            exists = UpdateQTraces(state, actions, action, reward);
+            QValue = QLearning(lastState, lastAction, state, action, reward);
+
+            TrainNueral(lastState, lastAction, QValue);
+
+            if (!exists)
+            {
+                traces.Add(new Trace(lastState.ToVector(), lastAction, 1));
+            }
+
+            lastAction = action;
+            lastState = state;
+
+            return action;
+        }
+
+        public Network GetNetwork()
+        {
+            return this.network;
+        }
+
+        public void SetNetwork(Network network)
+        {
+            this.network = network;
+        }
+
+        public double[] CreateInput(State state, string action)
+        {
+            List<double> input = new List<double>();
+
+            input.Add((double)(allActions.FindIndex(0, x => x == action) + 1) / 10);
+            foreach(float val in state.ToVector())
+            {
+                input.Add(Convert.ToDouble(val));
+            }
+
+            return input.ToArray();
+        }
+
+        public void TrainNueral(State state, string action, double output)
+        {
+            double[] input = CreateInput(state, action);
+            double[] tmp = { output };
+
+            TrainingSample sample = new TrainingSample(input, tmp);
+
+            network.Learn(sample, 0, epoch);
+        }
+
+        public string EpsilonGreedy(Dictionary<string, double> QValues, List<string> options)
+        {
+            string selectedAction = "";
+
+            Random rnd = new Random();
+            double val = rnd.NextDouble();
+
+            if(val >= this.epsilon)
+            {
+                selectedAction = FindMaxValues(QValues);
+            }
+            else
+            {
+                selectedAction = options[rnd.Next(0, options.Count)];
+            }
+
+            return selectedAction;
+        }
+
+        public string FindMaxValues(Dictionary<string, double> tempQ)
+        {
+            List<string> maxKeyValues = new List<string>();
+            double maxVal = -1;
+
+            foreach(KeyValuePair<string, double> kvp in tempQ)
+            {
+                if(kvp.Value > maxVal)
+                {
+                    maxKeyValues.Clear();
+                    maxVal = kvp.Value;
+                }
+                if(kvp.Value == maxVal)
+                {
+                    maxKeyValues.Add(kvp.Key);
+                }
+            }
+
+            Random rand = new Random();
+            string action = maxKeyValues[rand.Next(maxKeyValues.Count)];
+
+            return action;
+        }
+
+        public double QLearning(State lastState, string lastAction, State nextState, string bestAction, double reward)
+        {
+            double QValue = network.Run(CreateInput(lastState,lastAction)).First();
+            double previousQ = QValue;
+
+            double newQ = network.Run(CreateInput(nextState, bestAction)).First();
+
+            QValue += alpha * (reward + gamma * newQ - previousQ);
+
+            return QValue;
+        }
+
+        public Dictionary<string, double> CalculateQVaules(State state)
+        {
+            Dictionary<string, double> tempQ = new Dictionary<string, double>();
+            foreach (string action in allActions)
+            {
+                double[] input = CreateInput(state, action);
+                double value = network.Run(input)[0];
+
+                tempQ.Add(action, value);
+            }
+
+            return tempQ;
+        }
+
+        public bool UpdateQTraces(State state, string action, double reward)
+        {
+            bool found = false;
+        }
+
+        public bool CheckSimilar(State state1, State state2)
+        {
+            bool similar = true;
+
+            double[] state1Vector = state1.ToVector();
+            double[] state2Vector = state2.ToVector();
+
+            double moneyComp = Math.Abs(state1Vector[11] - state2Vector[11]) + Math.Abs(state1Vector[12] - state2Vector[12]);
+            if (moneyComp >= 0.1)
+            {
+                similar = false;
+            }
+
+
+        }
     }
 
     public class State
@@ -33,9 +249,9 @@ namespace Monopoly
             this.playerList = playerList;
         }
 
-        public float[] ToVector()
+        public double[] ToVector()
         {
-            float[] propertyValues = new float[10];
+            double[] propertyValues = new double[10];
             Dictionary<string, int> owned = new Dictionary<string, int>();
             foreach(Property prop in p.GetProperties())
             {
@@ -184,7 +400,7 @@ namespace Monopoly
             moneyProportion = p.GetMoney() / totalMoney;
             propertyProportion = totalOwned / p.GetProperties().Count;
 
-            float[] stateVector = new float[13];
+            double[] stateVector = new double[13];
             stateVector[0] = propertyValues[0];
             stateVector[1] = propertyValues[1];
             stateVector[2] = propertyValues[2];
---
+++ Monopoly/Trace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class Trace
+    {
+        public double[] state { get; set; }
+        public string action { get; set; }
+        public float value { get; set; }
+
+        public Trace(double[] state, string action, float value) 
+        {
+            this.state = state;
+            this.action = action;
+            this.value = value;
+        }
+
+    }
+}
